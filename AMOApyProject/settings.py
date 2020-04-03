fd3533f5b2156fb83d853892c98af6b8e478ab19
ILYASS_AMOA <17rm1420@gmail.com>
2020-04-03 23:11:22 +0100

    Added django-heroku

--- AMOApyProject/settings.py
+++ AMOApyProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -133,4 +133,6 @@ CRISPY_TEMPLATES_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL ='base-home'
 LOGIN_URL = 'login'
 
+
+django_heroku.settings(locals())
 
