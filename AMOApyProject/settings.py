3d6b652669da8a39db2f9269e27ccad655fc7f36
ILYASS_AMOA <17rm1420@gmail.com>
2020-04-04 09:59:28 +0100

    added statis to root

--- AMOApyProject/settings.py
+++ AMOApyProject/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-os.makedirs(STATIC_TMP, exist_ok=True)
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_TMP = os.path.join(BASE_DIR, 'static')
+#os.makedirs(STATIC_TMP, exist_ok=True)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 STATIC_ROOT=os.path.join(BASE_DIR , 'staticfiles')
